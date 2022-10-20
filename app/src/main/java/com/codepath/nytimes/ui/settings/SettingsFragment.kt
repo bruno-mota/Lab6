9dcb409a2104e87a45b64af1dbd710981892e234
Bruno Mota <bruno.ed.mota@gmail.com>
2022-10-20 17:30:06 -0400

    Checkpoint 4

---
+++ app/src/main/java/com/codepath/nytimes/ui/settings/SettingsFragment.kt
@@ -0,0 +1,40 @@
+package com.codepath.nytimes.ui.settings
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.codepath.nytimes.R
+
+class SettingsFragment: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings,container,false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+    }
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment BlankFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance():SettingsFragment {
+
+            return SettingsFragment()
+        }
+    }
+}
